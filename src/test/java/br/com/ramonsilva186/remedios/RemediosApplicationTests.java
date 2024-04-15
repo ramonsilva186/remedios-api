8c21149f8ba57d5481f6118c3f0390b0329ae2c7
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-15 12:29:10 -0300

    Project Created

---
+++ src/test/java/br/com/ramonsilva186/remedios/RemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ramonsilva186.remedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
